c34dc53c034f7c55be0a3ad6e5f1f25ab0f67291
Gabriel Faria <gabrielfaria88@gmail.com>
2024-05-20 22:19:11 -0300

    implementing useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { Flex, Heading, Input, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, Input, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount(count + 1)
+  }
+
   return (
     <Flex width='100vw' height='100vh' background='black' justifyContent='center' alignItems='center' flexDirection='column'>
       <Text color='white'>Olá mundo</Text>
       <Input focusBorderColor='lime' placeholder='Here is a sample placeholder' />
+      <Text color='white' fontSize='3xl'>{count}</Text>
+      <Button onClick={handleCount}>Aumentar 1</Button>
     </Flex>
   );
 }
